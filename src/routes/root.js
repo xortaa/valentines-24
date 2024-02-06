eb6eab35658702c53fa5e4b9a7a26d179357a202
xortaa <kercwinpaul@gmail.com>
2024-02-07 05:33:37 +0800

    <333

--- src/routes/root.js
+++ src/routes/root.js
@@ -7,7 +7,7 @@ export default function Root() {
   return (
     <div className='homePage'>
       <img
-        src={process.env.PUBLIC_URL + "/catshakira-cat.gif"}
+        src='https://scontent.xx.fbcdn.net/v/t39.1997-6/69037885_1459634297494642_2555601142797041664_n.webp?_nc_cat=103&ccb=1-7&_nc_sid=ba09c1&_nc_eui2=AeHkExaMXe-lufVZ1LGhViI2MiA2-eRFR2IyIDb55EVHYlPmKOGjowOYNzpW_HKbs8ScLZLXuJVuWruVxJNhP2TH&_nc_ohc=_IIkSXfA6DQAX8720Z-&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=00_AfB4fsS2ih4FZXaa0ibNNss9v_8NJ2hmrIz3YOVNyGylBg&oe=65C7093E'
         alt='shakira cat'
         className={hidden ? "cat" : "cat cat_hover"}
       />
--- src/routes/valentine.js
+++ src/routes/valentine.js
@@ -1,19 +1,44 @@
 import "../styles/valentine.css"
-import { useState } from "react"
+import { useState, useRef } from "react"
 import Love from "../components/Love"
+import YouTube from "react-youtube"
+
+const hiddenOpts = {
+  height: "1",
+  width: "1",
+  playerVars: {
+    autoplay: 0,
+  },
+}
+
+const visibleOpts = {
+  height: "390",
+  width: "640",
+  playerVars: {
+    autoplay: 1,
+  },
+}
 
 function ValentinePage() {
   const [click, setClick] = useState(false)
-  // play https://www.youtube.com/watch?v=_QGIzmtIr3E as music when clicked
+  const playerRef = useRef(null)
+
+  const onReady = (event) => {
+    playerRef.current = event.target
+  }
+
+  const handleClick = () => {
+    setClick(true)
+    // play the video
+    if (playerRef.current) {
+      playerRef.current.playVideo() // play the video
+    }
+  }
+
   return (
     <div className='valentine'>
       <div className={click ? "hidden" : "letter_container"}>
-        <img
-          src={process.env.PUBLIC_URL + "/letter.png"}
-          alt='letter'
-          onClick={() => setClick(true)}
-          className='letter'
-        />
+        <img src={process.env.PUBLIC_URL + "/letter.png"} alt='letter' onClick={handleClick} className='letter' />
         <i>click me</i>
       </div>
       <div className={click ? "secret" : "hidden"}>
@@ -29,13 +54,14 @@ function ValentinePage() {
             kasayahan kapag kasama ka; Na tumatahimik ang mundo at nakakalimutan ko ang mga problema ko. Nasasabik na
             ako sa mga plano natin, mga lugar na gustong puntahan, mga pagkain na gustong tikman. Parang adventure pag
             magkasama tayo na masaya akong kasama ka kahit anong mangyari na gusto kitang makasama sa lahat at sa habang
-            buhay. Napapasaya mo ako ng sobra ikaw ang aking lahat, ang aking layunin ang aking buhay ang aking mahal.
+            buhay. Napapasaya mo ako ng sobra ikaw ang aking lahat, ang aking layunin ang aking buhay ang aking mahal. Taging layunin ko lang sa buhay ay ang makasama ka at mapasaya ka. Mahal na mahal kita. Happy Valentine's Day my love.
           </i>
         </div>
         <div>
           <Love />
         </div>
       </div>
+      <YouTube videoId='PCQldI3q6yc' opts={click ? visibleOpts : hiddenOpts} onReady={onReady} />
     </div>
   )
 }
